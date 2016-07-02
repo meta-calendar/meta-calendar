6c27a87e7759baa57f0f0dff22d65a2255c20108
Olav Schettler <olav@schettler.net>
2016-07-02 16:01:58 +0200

    Domain model for events

---
+++ app/Domain/Event.php
@@ -0,0 +1,62 @@
+<?php
+
+class Event
+{
+    private $summary;
+    private $start;
+    private $end;
+    private $location;
+    private $url;
+
+    /**
+     * Event constructor.
+     * @param $summary
+     * @param $start
+     * @param $end
+     * @param $location
+     * @param $url
+     */
+    public function __construct($summary, $start, $end, $location, $url) {
+        $this->summary = $summary;
+        $this->start = $start;
+        $this->end = $end;
+        $this->location = $location;
+        $this->url = $url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSummary() {
+        return $this->summary;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStart() {
+        return $this->start;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEnd() {
+        return $this->end;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLocation() {
+        return $this->location;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUrl() {
+        return $this->url;
+    }
+
+}
