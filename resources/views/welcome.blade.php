0bdb2aa08f238016daf7fcfd51646990af95cb72
Olav Schettler <olav@schettler.net>
2016-07-02 16:49:06 +0200

    Mustache

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,11 +8,34 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-
+                    <ul id="events"></ul>
                     Your events
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.min.js"></script>
+
+<script id="event-template" type="template">
+<li>
+    <p>{{summary}}</p>
+    <p>{{start}} .. {{end}}</p>
+    <p><a href="{{url}}">Link</a></p>
+</li>
+</script>
+
+<script>
+    jQuery(function () {
+       $.get('/api/events', function (events) {
+           var $events = $('#events'),
+               template = $('#event-template').html();
+           for (event in events) {
+              $events.append(Mustache.render(template, event))
+           }
+       });
+    });
+</script>
+
 @endsection
